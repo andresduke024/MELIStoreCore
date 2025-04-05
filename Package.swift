7f0c1f265a962eec5592d1d9708b6d4a8db48c79
Andres Duque <andresduke024@gmail.com>
2025-04-05 14:21:00 -0500

    Add - Initial package definition

--- Package.swift
+++ Package.swift
@@ -5,20 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "MELIStoreCore",
+    platforms: [.iOS(.v15), .macOS(.v10_15), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MELIStoreCore",
             targets: ["MELIStoreCore"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/andresduke024/swift-dependency-injector", .upToNextMinor(from: "2.0.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MELIStoreCore"),
+            name: "MELIStoreCore",
+            dependencies: [
+                .product(name: "SwiftDependencyInjector", package: "swift-dependency-injector"),
+            ]
+        ),
         .testTarget(
             name: "MELIStoreCoreTests",
-            dependencies: ["MELIStoreCore"]
+            dependencies: [
+                "MELIStoreCore",
+                .product(name: "SwiftDependencyInjector", package: "swift-dependency-injector"),
+            ]
         ),
     ]
 )
